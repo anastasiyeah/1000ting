c5c013da0f7062a6a600f101a1be36366c72cdac
trinelangberg <trinelangberg@icloud.com>
2018-05-18 18:02:34 +0200

    Om os added

--- om_os.php
+++ om_os.php
@@ -6,11 +6,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <meta name="description" content="">
 
-<link href="css/index.css" type="text/css" rel="stylesheet"> 
+<link href="css/om_os.css" type="text/css" rel="stylesheet"> 
 <link href="css/styles_header.css" type="text/css" rel="stylesheet"> 
 	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500" rel="stylesheet">
 	
-<title>1000 TING | Jægergårdsgade 18-20, 8000 Århus C</title>
+<title>1000 TING | OM OS</title>
 
 </head>
 	
@@ -20,17 +20,10 @@
      <?php include "includes/header.php";?>
 
 	<!---- VIDEO ---->
-	
-	<section class="video">
-		<video></video>
-	</section>
+
 	
 	<!---- PRODUCTS AND LINK TO WEBSHOP ---->		
-		<section class="products">
-			<p class="products__description">Alle vores produkter er enten nøje udvalgt, fremstillet af genbrugsmaterialer, håndlavede unikaer eller fra en helt anden etnisk kultur. Fælles for alt er, at der er produkter med en historie og personlighed.</p>
-		</section>
-		
-	<a href="#" class="webshoplink" >GÅ TIL WEBSHOP</a>
+
 
 	
 	
@@ -39,9 +32,13 @@
 		<h3>Vi er mere end bare en butik!</h3>
 		<h4>Kom forbi og se for dig selv</h4>
 		<p>Mandag-fredag: kl.10.00 - 17.30 <br>
-		Lørdag: kl.10.00 - 14.00 · Søndag: lukket</p>
-		<!--- <address>Jægergårdsgade 18-20, 8000 Århus C</address> -->
+		Lørdag: kl.10.00 - 14.00 <br>
+		Søndag: lukket</p>
+		<address>Jægergårdsgade 18-20 <br>
+				8000 Århus C</address>
 	</section>
+	
+	
     
 </body>
 	
