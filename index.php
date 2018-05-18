67e1d29d5799f4cd2f3ec6ff734503363a78b9b9
trinelangberg <trinelangberg@icloud.com>
2018-05-18 13:09:23 +0200

    Starting up
    First push

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+	
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<meta name="description" content="">
+
+<link href="css/index.css" type="text/css" rel="stylesheet"> 
+	
+<title>1000 TING | Jægergårdsgade 18-20, 8000 Århus C</title>
+
+</head>
+	
+<body>
+
+	<header>
+	</header>
+	
+	<section>
+		<video></video>
+		
+		<article id="welcome">
+			<h2>Find din næste interiør-skat hos os, i centrum af Speltghettoen i Aarhus</h2>
+			<p>.. Vores produkter er alle eneten nøje udvalgt, fremstillet af genbrugsmaterialer, håndlavede unikaer eller fra en helt anden etnisk kultur. Fælles for alt er, at der er </p>
+		</article>
+		
+		<button><a href="#" >WEBSHOP</a></button>
+		
+	</section>
+    
+</body>
+	
+</html>
