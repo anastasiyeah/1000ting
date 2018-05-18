a41db04fe2150130c44bfd7a77e67d20d7b1cceb
anastasiyeah <anastasiyeah@Anastasiyas-MacBook-Pro.local>
2018-05-18 14:41:45 +0200

    Header.php
    Header in php includes

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!-- HEADER START -->
+
+    <nav class="navigation">
+       <h2 class="navigation__name">1000TING</h2>
+    
+        <ul class="navigation__line">
+            <li class="navigation__link"><a href="index.html">HJEM</a></li>
+            <li class="navigation__link"><a href="#">OM OS</a></li>
+            <li class="navigation__link"><a href="#">KONTAKT</a></li>
+            <li class="navigation__link"><a href="#">WEBSHOP</a></li>
+        </ul>
+     </nav>
+     
+     <div class="borderline"></div>
+
+<!-- HEADER END -->
+    
---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- mobile -->
+<title>1000 TING | Jægergårdsgade 18-20, 8000 Århus C</title>
+<meta name="description" content="">
+    
+    <link href="css/styles_header.css" rel="stylesheet" type="text/css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+	
+</head>
+ <body>
+     
+     <!--
+    
+	<nav class="navigation">
+       <h2 class="navigation__name">1000TING</h2>
+    
+        <ul class="navigation__line">
+            <li class="navigation__link"><a href="index.html">HJEM</a></li>
+            <li class="navigation__link"><a href="#">OM OS</a></li>
+            <li class="navigation__link"><a href="#">KONTAKT</a></li>
+            <li class="navigation__link"><a href="#">WEBSHOP</a></li>
+        </ul>
+     </nav>
+     
+     <div class="border"></div>
+    -->
+     
+     <?php include "header.php";?>
+     
+</body>
+</html>
