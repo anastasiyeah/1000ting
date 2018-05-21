9d69655bb7cdb90661cf1f82c9d0f75da2cf6622
anastasiyeah <anastasiyeah@Anastasiyas-MacBook-Pro.local>
2018-05-21 18:02:39 +0200

    adding Trine's files

---
+++ om_os.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+	
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<meta name="description" content="">
+
+<link href="css/om_os.css" type="text/css" rel="stylesheet"> 
+<link href="css/styles_header.css" type="text/css" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500" rel="stylesheet">
+	
+<title>1000 TING | OM OS</title>
+
+</head>
+	
+<body>
+	
+	   <!-- HEADER INCLUDE -->
+     <?php include "includes/header.php";?>
+
+	<!---- OM OS  ---->
+
+	<section class="about">
+		<article class="about__article">
+		<h1 class="about__h1">Meget mere end bare en butik</h1>
+		<p class="about__p">Alle vores produkter er enten nøje udvalgt, fremstillet af genbrugsmaterialer, håndlavede unikaer eller fra en helt anden etnisk kultur. Fælles for alt er, at det er produkter med en historie og en personlighed.Alle vores produkter er enten nøje udvalgt, fremstillet af genbrugsmaterialer, håndlavede unikaer eller fra en helt anden etnisk kultur. Fælles for alt er, at det er produkter med en historie og en personlighed.</p>
+		</article>
+	<img class="about__img" src="images/facade.jpg" alt="Picture of 1000Tings shop facade" >
+	</section>
+	
+	
+	
+	
+	<!---- OPENING HOURS ---->		
+	<section class="openinghours">
+		<h4 class="openinghours__h4">Åbningstider</h4>
+		<p class="openinghours__p">Mandag-fredag: kl.10.00 - 17.30 <br>
+		Lørdag: kl.10.00 - 14.00 <br>
+		Søndag: lukket</p>
+		
+		<address class="openinghours__address">Jægergårdsgade 18-20 <br>
+				8000 Århus C</address>		
+	</section>
+	
+	
+    
+</body>
+	
+</html>
