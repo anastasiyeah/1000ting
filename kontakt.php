81262a7a68e35bf49f548de75d7d89618b024b7f
Annaaakb <36158732+Annaaakb@users.noreply.github.com>
2018-05-21 11:37:15 +0200

    Oprettelse af kontaktside samt styles

---
+++ kontakt.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- mobile -->
+<title>1000 TING | Jægergårdsgade 18-20, 8000 Århus C</title>
+<meta name="description" content="">
+    
+    <link href="css/styles_header.css" rel="stylesheet" type="text/css">
+    
+    <link href="css/styles_kontakt.css" rel="stylesheet" type="text/css">
+    
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+	
+</head>
+ <body>
+     
+     <!-- HEADER INCLUDE -->
+     <?php include "includes/header.php";?>
+     
+     <section class="contact">
+     <section class="contact_header">
+     <h1>KONTAKT OS</h1>
+     <p>Vi glæder os til at høre fra dig eller se dig i butikken.</p>
+        </section>
+         
+         </section>
+     
+     
+     
+</body>
+</html>
