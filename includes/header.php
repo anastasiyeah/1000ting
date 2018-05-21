20d3a4eaf86f073896b45abb2b3986a69ba64cf0
anastasiyeah <anastasiyeah@Anastasiyas-MacBook-Pro.local>
2018-05-21 17:03:59 +0200

    header #

--- includes/header.php
+++ includes/header.php
@@ -4,10 +4,10 @@
        <h2 class="navigation__name">1000TING</h2>
     
         <ul class="navigation__line">
-            <li class="navigation__link"><a href="index.html">HJEM</a></li>
-            <li class="navigation__link"><a href="#">OM OS</a></li>
+            <li class="navigation__link"><a href="index.php">HJEM</a></li>
+            <li class="navigation__link"><a href="om_os.php">OM OS</a></li>
             <li class="navigation__link"><a href="kontakt.php">KONTAKT</a></li>
-            <li class="navigation__link"><a href="#">WEBSHOP</a></li>
+            <li class="navigation__link"><a href="webshop.php">WEBSHOP</a></li>
         </ul>
      </nav>
      
