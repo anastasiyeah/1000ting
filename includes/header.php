75b7dda01625726706d24fdf40dda87a0e0c55d4
anastasiyeah <anastasiyeah@Anastasiyas-MacBook-Pro.local>
2018-05-23 15:05:51 +0200

    header

--- includes/header.php
+++ includes/header.php
@@ -8,13 +8,14 @@
             <li class="navigation__link"><a href="kontakt.php">KONTAKT</a></li>
             <li class="navigation__link"><a href="webshop.php">WEBSHOP</a></li>
         </ul>
+        <ion-icon class="shoppingcart" name="cart"></ion-icon>
+
+    <p class="available_languages">DA/EN</p>
      </nav>
      
    <!--  <div class="borderline"></div> -->
 
-    <ion-icon class="shoppingcart" name="cart"></ion-icon>
-
-    <p class="available_languages">DA/EN</p>
+    
 
      
 <!-- HEADER END -->
