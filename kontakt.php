1d472395effc51dec3f2c9ef5f6c2adc8ebdb403
Annaaakb <36158732+Annaaakb@users.noreply.github.com>
2018-05-23 21:47:51 +0200

    css variable - hover on socialmedia link

--- kontakt.php
+++ kontakt.php
@@ -33,14 +33,14 @@
     <img class="contact__img3" src="images/blueflowers1.jpg" alt="Blue flowers from shop">
     
          <section class="contact__info">
-             <h2>ADRESSE</h2>
+             <h2 class="contact__info__headline">ADRESSE</h2>
              <p>1000TING<br>Jægergårdsgade 18-20<br>8000 Aarhus C</p>
              <h2>KONTAKT</h2>
              <p>+45 86 18 10 90<br>butik1000ting@gmail.com</p>
              <h2>ÅBNINGSTIDER</h2>
              <p>Mandag: 10.00-17.30<br>Tirsdag: 10.00-17.30<br>Onsdag: 10.00-17.30<br>Torsdag: 10.00-17.30<br>Fredag: 10.00-18.00<br>Lørdag: 10.00-14.00<br>Søndag: Lukket</p>
              <h2>SOCIALE MEDIER</h2>
-             <i class="fa fa-facebook-official" aria-hidden="true"></i> <i class="fa fa-instagram" aria-hidden="true"></i>
+             <a href="https://www.facebook.com/1000-TING-Jægergårdsgade-18-20-8000-Århus-C-160721700633324/" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true" ></i></a> <a href="https://www.instagram.com/butik1000ting/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
          </section>
          
          </section>
