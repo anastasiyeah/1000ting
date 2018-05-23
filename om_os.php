5f15ecfb74ae7cdd6ad52d1c2e4cdb8326ad1d3f
Annaaakb <36158732+Annaaakb@users.noreply.github.com>
2018-05-23 12:20:12 +0200

    Styling + implementing picture & animation

--- om_os.php
+++ om_os.php
@@ -14,9 +14,12 @@
 	
 <title>1000 TING | OM OS</title>
 
+    <script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
+    <script src="js/animationflower.js"></script>
+    <script src="js/animation.js"></script>
 </head>
 	
-<body>
+<body onload="init();">
 	
 	   <!-- HEADER INCLUDE -->
      <?php include "includes/header.php";?>
@@ -25,26 +28,30 @@
 
 	<section class="about">
 		<article class="about__article">
-		<h1 class="about__h1">Meget mere end bare en butik</h1>
-		<p class="about__p">Med en lang historie i Jægergårdsgade, der strækker sig over mere end 25 år, har Vi igennem årene fået skabt et kreativt og hyggeligt miljø i butikken, der indbyder kunderne til, at gå på opdagelse i vores stemningsfyldte, specielle butik.
+		<h1 class="about__headline">Meget mere end bare en butik</h1>
+		<p class="about__maintext">Med en lang historie i Jægergårdsgade, der strækker sig over mere end 25 år, har Vi igennem årene fået skabt et kreativt og hyggeligt miljø i butikken, der indbyder kunderne til, at gå på opdagelse i vores stemningsfyldte, specielle butik.
 			Det summende gadeliv, Jægergårdsgade har, er med til at skabe en helt specielt og hyggelig stemning, som gennemstrømmer butikken - <br> Vi er en del af Jægergårdsgade - Jægergårdsgade er en del af os	</p>
 		</article>
-	<img class="about__img" src="images/facade.jpg" alt="Picture of 1000Tings shop facade" >
+	<img class="about__img" src="images/hangingplants.jpg" alt="Hanging plants Butik 1000Ting" >
 	</section>
 	
+    <!-- Flower animation -->
+	<div id="animation_container" >
+		<canvas id="canvas" width="500px"></canvas>
+		<div id="dom_overlay_container">
+		</div>
+	</div>
 	
 	
-	
-	<!---- OPENING HOURS ---->		
-	<section class="openinghours">
-		<h4 class="openinghours__h4">Åbningstider</h4>
-		<p class="openinghours__p">Mandag-fredag: kl.10.00 - 17.30 <br>
-		Lørdag: kl.10.00 - 14.00 <br>
-		Søndag: lukket</p>
-		
-		<address class="openinghours__address">Jægergårdsgade 18-20 <br>
-				8000 Århus C</address>		
+	<!---- Footer Gallery ---->		
+	<section class="gallery">
+        <h1 class="gallery__headline">KOM IND OG MÆRK STEMNINGEN</h1>
+		<img class="gallery__img" src="images/buddha.jpg" alt="Buddha decoration">
+        <img class="gallery__img" src="images/feather.jpg" alt="Decorations feather">
+        <img class="gallery__img" src="images/postcard.jpg" alt="Postcard Frida Carlo">
 	</section>
+    
+    
 	
 	   <!-- Ionicons -->
      <script src="https://unpkg.com/ionicons@4.1.1/dist/ionicons.js"></script>
