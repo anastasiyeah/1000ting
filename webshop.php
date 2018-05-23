dfac57d7ff2a314573da8f6edf2da47e2018e9b8
anastasiyeah <anastasiyeah@Anastasiyas-MacBook-Pro.local>
2018-05-23 13:27:31 +0200

    webshop
    webshop product list items

--- webshop.php
+++ webshop.php
@@ -22,7 +22,7 @@
      <!-- Webshop picture banner -->
      <section class="webshop">
          <section class="webshop__banner">
-             <img src="images/webshop_banner.jpg" width="100%">
+             <img class="webshop__banner-image" src="images/webshop_banner.jpg">
              <h1 class="webshop__header">1000TING'S WEBSHOP</h1>
              <p class="webshop__subtext">Cae nam fugiaepturem voles dunt magnis apicim quo</p>
          </section>
@@ -58,24 +58,36 @@
          <section id="main" class="webshop__products">
              <ul class="webshop__list">
                  <li class="webshop__items">
-                     <img src="images/keramik1.png" width="60%" alt="">
+                     <img class="webshop__image" src="images/keramik1.png" alt="">
                      <h2>Unika Keramik <br> 250DKK</h2>
-                     <span style="cursor:pointer" onclick="openNav()">info</span>
+                     <!--<span style="cursor:pointer" onclick="openNav()">info</span> -->
                  </li>
                  <li class="webshop__items">
-                     <img src="images/keramik2.png" width="60%" alt="">
+                     <img class="webshop__image" src="images/keramik2.png" alt="">
                      <h2>Unika Keramik <br> 250DKK</h2>
                  </li>
                   <li class="webshop__items">
-                     <img src="images/keramik2.png" width="60%" alt="">
+                     <img class="webshop__image" src="images/keramik2.png" alt="">
                      <h2>Unika Keramik <br> 250DKK</h2>
                  </li>
                   <li class="webshop__items">
-                     <img src="images/keramik2.png" width="60%" alt="">
+                     <img class="webshop__image" src="images/keramik2.png" alt="">
                      <h2>Unika Keramik <br> 250DKK</h2>
                  </li>
                   <li class="webshop__items">
-                     <img src="images/keramik2.png" width="60%" alt="">
+                     <img class="webshop__image" src="images/keramik2.png" alt="">
+                     <h2>Unika Keramik <br> 250DKK</h2>
+                 </li>
+                  <li class="webshop__items">
+                     <img class="webshop__image" src="images/keramik2.png" alt="">
+                     <h2>Unika Keramik <br> 250DKK</h2>
+                 </li>
+                  <li class="webshop__items">
+                     <img class="webshop__image" src="images/keramik2.png" alt="">
+                     <h2>Unika Keramik <br> 250DKK</h2>
+                 </li>
+                  <li class="webshop__items">
+                     <img class="webshop__image" src="images/keramik2.png" alt="">
                      <h2>Unika Keramik <br> 250DKK</h2>
                  </li>
              </ul>
@@ -88,54 +100,24 @@
              <p>info about the product</p>
         </div>
          -->
-         
-         <div class="container demo">
-	
-	<div class="text-center">
-		<button type="button" class="btn btn-demo" data-toggle="modal" data-target="#myModal2">
-			Right Sidebar Modal
-		</button>
-	</div>
-	
-	<!-- Modal -->
-	<div class="modal right fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel2">Right Sidebar</h4>
-				</div>
-
-				<div class="modal-body">
-					<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-					</p>
-				</div>
-
-			</div><!-- modal-content -->
-		</div><!-- modal-dialog -->
-	</div><!-- modal -->
-	
-	
-</div><!-- container -->
 
          
      </section>
 
      <!--
-<script>
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+    <script>
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-}
-</script>
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft= "0";
+            document.body.style.backgroundColor = "white";
+    }
+    </script>
 -->
 
      <script src="js/filter.js"></script>
