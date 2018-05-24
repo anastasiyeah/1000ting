f5e3548f3960725920fe3f50d0f4d4079ab23b34
anastasiyeah <anastasiyeah@Anastasiyas-MacBook-Pro.local>
2018-05-24 15:16:35 +0200

    header with correct links

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,8 @@
         <ul class="navigation__line">
             <h2 class="navigation__name">BUTIK 1000TING</h2>
             <li class="navigation__link"><a href="index.php">HJEM</a></li>
-            <li class="navigation__link"><a href="om_os.php">OM OS</a></li>
-            <li class="navigation__link"><a href="kontakt.php">KONTAKT</a></li>
+            <li class="navigation__link"><a href="about.php">OM OS</a></li>
+            <li class="navigation__link"><a href="contact.php">KONTAKT</a></li>
             <li class="navigation__link"><a href="webshop.php">WEBSHOP</a></li>
         </ul>
      </nav>
