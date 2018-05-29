815a70c62f4d53b56b3f577b76aea4cde87140f5
Annaaakb <36158732+Annaaakb@users.noreply.github.com>
2018-05-29 09:26:58 +0200

    Header original chages

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,19 @@
-<!-- HEADER START -->
+<!-- header navigation -->
 
     <nav class="navigation">
         <ul class="navigation__line">
-            <li><h2 class="navigation__name">BUTIK 1000TING</h2></li>
+            <li class="navigation__name">BUTIK 1000TING</li>
             <li class="navigation__link"><a href="index.php">HJEM</a></li>
-            <li class="navigation__link"><a href="om_os.php">OM OS</a></li>
-            <li class="navigation__link"><a href="kontakt.php">KONTAKT</a></li>
+            <li class="navigation__link"><a href="about.php">OM OS</a></li>
+            <li class="navigation__link"><a href="contact.php">KONTAKT</a></li>
             <li class="navigation__link"><a href="webshop.php">WEBSHOP</a></li>
         </ul>
      </nav>
-     
-    <div class="borderline"></div>
 
+<!-- shopping cart -->
     <ion-icon class="shoppingcart" name="cart"></ion-icon>
 
+<!--DA/EN -->
     <p class="available_languages">DA/EN</p>
 
-     
-<!-- HEADER END -->
     
